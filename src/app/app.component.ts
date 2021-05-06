cc2861bdd332b3fbeea294827e62e01dabeef98d
srsexton94 <srsexton94@gmail.com>
2021-05-05 20:04:14 -0400

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="toolbar" role="banner">
+      <h1>{{ title }} app is running!</h1>
+    </div>
+  `
 })
 export class AppComponent {
   title = 'marketplace';
